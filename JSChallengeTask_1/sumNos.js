44432e0035ae75f532d29cf3362f71a4011bb97a
Oluwafunmike Akinsanmi <fmsfunky@gmail.com>
2024-02-28 01:14:49 +0100

    Java Coding Challenge Task1

---
+++ JSChallengeTask_1/sumNos.js
@@ -0,0 +1,18 @@
+
+let result=0;
+
+const myArraySum = [10, 20,30,40,50,60,70,80]
+
+// initialise a variable for the sum//
+
+for (let sumNo=0; sumNo < myArraySum.length; sumNo ++){
+
+    result = result + (myArraySum[sumNo]);
+    
+}
+//Print result here
+
+console.log ("Total Sum in Array is " + result);
+
+
+console.log(myArraySum.length)
